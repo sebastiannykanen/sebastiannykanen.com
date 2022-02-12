e69bd15f8c101ecf82bd844bb276c10cf9b7138d
Sebastian Nykänen <sebastian.nykanen@outlook.com>
2022-02-12 11:19:05 +0200

    changed about me text

--- src/About.js
+++ src/About.js
@@ -12,7 +12,7 @@ function About(props) {
       <h1 id="AboutMe">
         Hello, my name is Sebastian. I'm an aspiring web developer living in
         Helsinki, Finland. I studied Industrial Engineering and Management in
-        Vasa, but now my goal is to become a self taught programmer. As of this
+        Vasa, but my goal is to become a self taught programmer. As of this
         moment, I have base knowledge in HTML, CSS, Javascript and React.
       </h1>
       <div></div>
