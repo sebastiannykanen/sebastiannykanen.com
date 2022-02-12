19c94d9cfe908868e9c17a50ccbc83cdeda1a230
Sebastian Nykänen <sebastian.nykanen@outlook.com>
2022-02-12 11:14:11 +0200

    Updated about page

--- src/About.js
+++ src/About.js
@@ -2,8 +2,20 @@ import React from "react";
 
 function About(props) {
   return (
-    <div id="App-main">
-      <h1>hej</h1>
+    <div id="About-main">
+      <div></div>
+      <img
+        alt="Picture of Sebastian Nykänen"
+        id="profile-picture"
+        src="/profilepicture.jpeg"
+      />
+      <h1 id="AboutMe">
+        Hello, my name is Sebastian. I'm an aspiring web developer living in
+        Helsinki, Finland. I studied Industrial Engineering and Management in
+        Vasa, but now my goal is to become a self taught programmer. As of this
+        moment, I have base knowledge in HTML, CSS, Javascript and React.
+      </h1>
+      <div></div>
     </div>
   );
 }
