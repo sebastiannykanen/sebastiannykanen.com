3a5c8aac23ef7326b1070fbf90ec1094af4b0951
Sebastian Nykänen <sebastian.nykanen@outlook.com>
2022-03-01 12:58:39 +0200

    mobile layout changes

--- src/Projects.js
+++ src/Projects.js
@@ -6,9 +6,21 @@ function Projects(props) {
   return (
     <AnimatedPage>
       <div id="Projects-main">
-        <h2>Project</h2>
-        <h2>Project</h2>
-        <h2>Project</h2>
+        <div className="project-card card1">
+          <a href="https://weather-app.sebastiannykanen.com/">
+            <h2>React Weather App</h2>
+          </a>
+        </div>
+        <div className="project-card card2">
+          <a href="https://weather-app.sebastiannykanen.com/">
+            <h2>Project</h2>
+          </a>
+        </div>
+        <div className="project-card card3">
+          <a href="https://weather-app.sebastiannykanen.com/">
+            <h2>Project</h2>
+          </a>
+        </div>
       </div>
     </AnimatedPage>
   );
