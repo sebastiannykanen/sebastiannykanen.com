a6f4be9e06656365c8a1477e06a72c46edcc7e9f
Sebastian Nykänen <sebastian.nykanen@outlook.com>
2022-04-19 13:06:21 +0300

    updated about me page with GitHub link

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,13 @@ function About(props) {
         Hello, my name is Sebastian. I'm an aspiring web developer living in
         Helsinki, Finland. I studied Industrial Engineering and Management in
         Vasa, but my goal is to become a self taught programmer. As of this
-        moment, I have base knowledge in HTML, CSS, Javascript and React.
+        moment, I have base knowledge in HTML, CSS, Javascript and React. Under
+        the "projects" section you can find my completed projects, but for a
+        closer look at my past and current projects, check out my{" "}
+        <a className="githubLink" href="https://github.com/sebastiannykanen">
+          GitHub
+        </a>{" "}
+        page.
       </h1>
       <div></div>
     </div>
