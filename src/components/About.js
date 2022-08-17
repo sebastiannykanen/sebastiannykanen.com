f69ae59e1856d92aa43b5d90af999ff280944aac
Sebastian Nykänen <sebastian.nykanen@outlook.com>
2022-08-17 15:36:46 +0300

    updated About page from Helsinki to Vasa

--- src/components/About.js
+++ src/components/About.js
@@ -12,11 +12,11 @@ function About(props) {
       />
       <h1 id="AboutMe">
         Hello, my name is Sebastian. I'm an aspiring web developer living in
-        Helsinki, Finland. I studied Industrial Engineering and Management in
-        Vasa, but my goal is to become a self taught programmer. As of this
-        moment, I have base knowledge in HTML, CSS, Javascript and React. Under
-        the "projects" section you can find my completed projects, but for a
-        closer look at my past and current projects, check out my{" "}
+        Vaasa, Finland. I studied Industrial Engineering and Management, but my
+        goal is to become a self taught programmer. As of this moment, I have
+        base knowledge in HTML, CSS, Javascript and React. Under the "projects"
+        section you can find my completed projects, but for a closer look at my
+        past and current projects, check out my{" "}
         <a className="githubLink" href="https://github.com/sebastiannykanen">
           GitHub
         </a>{" "}
